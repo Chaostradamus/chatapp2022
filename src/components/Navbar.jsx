5d737d9b4b00dde2685c4d027f163d89dcbf168e
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2022-09-17 15:04:08 -0400

    precoloring

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,16 @@
+import React, { useContext } from "react";
 import { signOut } from "firebase/auth";
-import React from "react";
 import { auth } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
-  const {currentUser} = useContext(AuthContext)
+  const { currentUser } = useContext(AuthContext);
 
   return (
     <div className="navbar">
-      <span className="logo">B Chao</span>
+      <span className="logo">Chao Chat</span>
       <div className="user">
-        <img
-          src={currentUser.photoURL}
-          alt=""
-        />
+        <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
         <button onClick={() => signOut(auth)}>logout</button>
       </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import Add from "../img/addAvatar.png";
-import {  createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
-function Login() {
+const Login = () => {
   const [err, setErr] = useState(false);
   const navigate = useNavigate();
 
@@ -13,30 +13,29 @@ function Login() {
     const password = e.target[1].value;
 
     try {
-     await createUserWithEmailAndPassword(auth, email, password);
-     navigate('/')
-    } catch (er) {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (err) {
       setErr(true);
     }
   };
-
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">B Chat</span>
-        <span className="title">Register</span>
+        <span className="logo">Chao Chat</span>
+        <span className="title">Login</span>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="display name" />
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
-
-          <button>Sign In</button>
+          <button>Sign in</button>
           {err && <span>Something went wrong</span>}
         </form>
-        <p>Have an account already? <Link to='/register'>Register</Link> </p>
+        <p>
+          You don't have an account? <Link to="/register">Register</Link>
+        </p>
       </div>
     </div>
   );
-}
+};
 
 export default Login;
